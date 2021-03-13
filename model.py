0c16b3308f3e3832f70029043da2f8e3ba563e9f
Viet Pham <vietuet98@gmail.com>
2021-03-13 21:20:33 +0700

    fix train.py

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ class RecSysModel(torch.nn.Module):
         self.d_type = d_type
 
         # initialized adjacency matrix
-        self.C = torch.from_numpy(adj_matrix.to_dense())
+        self.C = torch.from_numpy(adj_matrix)
         # values = adj_matrix.data
         # indices = np.vstack((adj_matrix.row, adj_matrix.col))
         #
